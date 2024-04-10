3cec7f74a29944f339e5e02999666629fe4695e5
Daniel Magana Camargo <s105934@ltisdschools.net>
2024-04-10 11:12:56 -0500

    Added basic movement controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed=5;
+    public float maxSpeed=5;
+    public float thrust = 5;
     public float jumpStrength=5;
     bool inAir=false;
 
     // Update is called once per frame
     void Update()
     {
-        inAir = (rb.velocityY != 0);
+        inAir = (rb.velocityY < -.01) || (rb.velocityY > .01);
         if (Input.GetKey("a") && !Input.GetKey("d"))
         {
-            rb.AddForceX(speed * -1, ForceMode2D.Force);
+            if(!(rb.velocityX < (-1 * maxSpeed)))
+                rb.AddForceX(thrust * -1, ForceMode2D.Force);
+            
         }
         else if (Input.GetKey("d") && !Input.GetKey("a"))
         {
-            rb.AddForceX(speed, ForceMode2D.Force);
+            if (!(rb.velocityX > maxSpeed))
+                rb.AddForceX(thrust, ForceMode2D.Force);
         }
         if (Input.GetKey("w")&& !inAir){
             rb.AddForceY(jumpStrength, ForceMode2D.Impulse);
